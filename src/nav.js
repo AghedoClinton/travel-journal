2555d291e25c2a12ea1eb0886c234077a649e9fc
Clinton Aghedo <aghedoclinton76@gmail.com>
2023-02-17 13:07:31 -0400

    Delete nav.js

--- src/nav.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Title(){
-    return(<div  className="overview">
-    <img className="img1"  src='./image/Fill213.png' alt /><h3 className="nav">My travel journal.</h3>
-    </div>)
-  }
